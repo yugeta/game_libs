2ee58729e928bd35515cd2cab885e3b0e0df7dcf
Yugeta.Koji <geta1972@gmail.com>
2024-02-14 17:45:14 +0900

    add data,preview

---
+++ preview/js/objects.js
@@ -0,0 +1,9 @@
+import * as THREE      from "three"
+import { Data }        from "./data.js"
+import { Render }      from "./render.js"
+
+export class Objects{
+	constructor(){
+		console.log(Render.scene.children)
+	}
+}
